d2c1887ab842f33b704270ec7cd5f2393be78c7a
MaxZar24 <maxymzaryshniak@gmail.com>
2023-12-06 17:45:03 +0200

    fix: fetching data from DB to profile

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,75 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const oracledb = require('oracledb')
 const app = express()
-const cors = require('cors');
 
+const dbConfig = {
+    user: 'mkr',
+    password: '2404',
+    connectString: 'localhost:1521',
+};
 
-app.use(cors());
-app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
-app.get('/api/checkConnection', (req, res) => {
-    res.json({ connected: true });
+app.post('/signup', async (req, res) => {
+    const {username, email, password} = req.body;
+
+    try {
+        const connection = await oracledb.getConnection(dbConfig);
+        const check_user = await connection.execute(
+            `SELECT * FROM users WHERE email = :1 AND password = :2`,
+            [email, password]
+        );
+
+        if (check_user.rows.length > 0) {
+            res.status(400).json({error: 'Can`t create new user! Email already used.'});
+        } else {
+            await connection.execute(
+                `INSERT INTO users (username, email, password) VALUES (:1, :2, :3)`,
+                [username, email, password],
+                {autoCommit: true}
+            );
+            await connection.close();
+            res.status(200).json({message: 'Data received and stored successfully!'});
+        }
+
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
 });
+
+app.post('/login', async (req, res) => {
+        const {email, password} = req.body;
+
+        try {
+            const connection = await oracledb.getConnection(dbConfig);
+            const result = await connection.execute(
+                `SELECT * FROM users WHERE email = :email AND password = :password`,
+                {email, password}
+            );
+
+            if (result.rows.length > 0) {
+                const userData = {
+                    username: result.rows[0][0],
+                    email: result.rows[0][1],
+                    password: result.rows[0][2],
+                };
+                res.status(200).json({message: 'Success logging', user: userData});
+            } else {
+                res.status(400).json({error: 'Incorrect login or password'});
+            }
+
+            await connection.close();
+        } catch
+            (error) {
+            console.error('Error:', error.message);
+            res.status(500).send('Internal Server Error');
+        }
+    }
+);
+
+
 
 app.listen(3001, () => {
     console.log("Server started success on port 3001")
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,100 +1,137 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
 
 const Home = () => {
     const [page, setPage] = useState('orders');
-    const [username, setUsername] = useState('blablabla');
-    const [password, setPassword] = useState('12345678');
+    const [userData, setUserData] = useState(null);
+    const [newUsername, setNewUsername] = useState('');
+    const [newPassword, setNewPassword] = useState('');
 
-    const changeUsername = () => {
-        alert('Username has been changed');
+    const storedUserData = sessionStorage.getItem('user');
+
+    useEffect(() => {
+        if (storedUserData) {
+            const parsedUserData = JSON.parse(storedUserData);
+            setUserData(parsedUserData);
+        }
+    }, [storedUserData]);
+
+    const changeUsername = async () => {
+        try {
+            const response = await axios.post('/change-username', {
+                userId: userData.id, // Assuming you have a unique user ID
+                newUsername: newUsername,
+            });
+
+            if (response.status === 200) {
+                alert('Username has been changed');
+                // You might want to update the user data in state or sessionStorage here
+            }
+        } catch (error) {
+            console.error('Error changing username:', error.message);
+            // Handle error
+        }
     };
 
-    const changePassword = () => {
-        alert('Password has been changed');
+    const changePassword = async () => {
+        try {
+            const response = await axios.post('/change-password', {
+                userId: userData.id, // Assuming you have a unique user ID
+                newPassword: newPassword,
+            });
+
+            if (response.status === 200) {
+                alert('Password has been changed');
+                // You might want to update the user data in state or sessionStorage here
+            }
+        } catch (error) {
+            console.error('Error changing password:', error.message);
+            // Handle error
+        }
     };
 
     return (
-        <div className="d-flex flex-column justify-content-center align-items-center bg-secondary vh-100">
-            <div>
-                <div className="btn-group w-100">
-                    <button
-                        onClick={() => setPage('profile')}
-                        className={`btn btn-primary rounded-bottom-0 ${page === 'profile' ? 'active' : ''}`}
-                    >
-                        Profile
-                    </button>
-                    <button
-                        onClick={() => setPage('orders')}
-                        className={`btn btn-primary rounded-bottom-0 ${page === 'orders' ? 'active' : ''}`}
-                    >
-                        Orders
-                    </button>
+            <div className="d-flex flex-column justify-content-center align-items-center bg-secondary vh-100">
+                <div>
+                    <div className="btn-group w-100">
+                        <button
+                            onClick={() => setPage('profile')}
+                            className={`btn btn-primary rounded-bottom-0 ${page === 'profile' ? 'active' : ''}`}
+                        >
+                            Profile
+                        </button>
+                        <button
+                            onClick={() => setPage('orders')}
+                            className={`btn btn-primary rounded-bottom-0 ${page === 'orders' ? 'active' : ''}`}
+                        >
+                            Orders
+                        </button>
 
+                    </div>
+
+                    {page === 'profile' ? (
+                        <div className="bg-white p-3 rounded-bottom">
+                            <h1>Profile</h1>
+                            <div><span className='fw-bold'>Email: </span>{userData.email}</div>
+                            <div>
+                                <label className="fw-bold">Username: </label>
+                                <input
+                                    type="text"
+                                    value={userData.username}
+                                    onChange={(e) => setNewUsername(e.target.value)}
+                                    id="newUsername"
+                                    name="newUsername"
+                                    className="form-control mb-3"
+                                />
+                                <button className="btn btn-secondary mb-3" onClick={changeUsername}>
+                                    Change username
+                                </button>
+                            </div>
+                            <div>
+                                <label className="fw-bold">Password: </label>
+                                <input
+                                    type="password"
+                                    value={userData.password}
+                                    onChange={(e) => setNewPassword(e.target.value)}
+                                    id="newPassword"
+                                    name="newPassword"
+                                    className="form-control mb-3"
+                                />
+                                <button className="btn btn-secondary mb-2" onClick={changePassword}>
+                                    Change password
+                                </button>
+                            </div>
+                        </div>
+                    ) : (
+                        <div className="bg-white p-3 rounded-bottom">
+                            <h1>Orders</h1>
+                            <table className="table">
+                                <thead>
+                                <tr>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Date</th>
+                                    <th scope="col">Category</th>
+                                    <th scope="col">Price</th>
+                                    <th scope="col">Amount</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Pen</td>
+                                    <td>25.11.2023</td>
+                                    <td>Stationery</td>
+                                    <td>5$</td>
+                                    <td>10</td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    )}
                 </div>
 
-                {page === 'profile' ? (
-                    <div className="bg-white p-3 rounded-bottom">
-                        <h1>Profile</h1>
-                        <div><span className='fw-bold'>Email: </span>example@gmail.com</div>
-                        <div>
-                            <label className="fw-bold">Username: </label>
-                            <input
-                                type="text"
-                                value={username}
-                                onChange={(e) => setUsername(e.target.value)}
-                                id="email"
-                                name="email"
-                                className="form-control mb-3"
-                            />
-                            <button className="btn btn-secondary mb-3" onClick={changeUsername}>
-                                Change username
-                            </button>
-                        </div>
-                        <div>
-                            <label className="fw-bold">Password: </label>
-                            <input
-                                type="password"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                id="password"
-                                name="password"
-                                className="form-control mb-3"
-                            />
-                            <button className="btn btn-secondary mb-2" onClick={changePassword}>
-                                Change password
-                            </button>
-                        </div>
-                    </div>
-                ) : (
-                    <div className="bg-white p-3 rounded-bottom">
-                        <h1>Orders</h1>
-                        <table className="table">
-                            <thead>
-                            <tr>
-                                <th scope="col">ID</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Date</th>
-                                <th scope="col">Category</th>
-                                <th scope="col">Price</th>
-                                <th scope="col">Amount</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Pen</td>
-                                <td>25.11.2023</td>
-                                <td>Stationery</td>
-                                <td>5$</td>
-                                <td>10</td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                )}
             </div>
-
-        </div>
     );
 };
 
