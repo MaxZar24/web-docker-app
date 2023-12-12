d7314249d5d8a1d152c509558d2117bb6e804226
MaxZar24 <maxymzaryshniak@gmail.com>
2023-12-12 23:50:26 +0200

    fix: changed DB

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const oracledb = require('oracledb')
+const mysql = require('mysql2/promise');
 const mongoose = require('mongoose');
 const Order = require("./models");
-const cors = require('cors');
 
 const app = express()
 
-const url = 'mongodb+srv://admin:1234@cluster0.smqleka.mongodb.net/orders?retryWrites=true&w=majority';
+const url = 'mongodb://172.17.0.5:27017/orders?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1';
 
-const dbConfig = {
+const mysqlConfig = {
+    host: 'localhost:3036',
     user: 'test',
-    password: '1',
-    connectString: 'localhost:1521',
+    password: '1234',
+    database: 'usersdb',
 };
 
 mongoose
@@ -24,83 +24,81 @@ mongoose
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+
+const pool = mysql.createPool(mysqlConfig);
+
 app.post('/signup', async (req, res) => {
-    const {username, email, password} = req.body;
+    const { username, email, password } = req.body;
 
     try {
-        const connection = await oracledb.getConnection(dbConfig);
-        const check_user = await connection.execute(
-            `SELECT * FROM users WHERE email = :1 AND password = :2`,
+        const connection = await pool.getConnection();
+        const [rows] = await connection.execute(
+            'SELECT * FROM users WHERE email = ? AND password = ?',
             [email, password]
         );
 
-        if (check_user.rows.length > 0) {
-            res.status(400).json({error: 'Can`t create new user! Email already used.'});
+        if (rows.length > 0) {
+            res.status(400).json({ error: 'Can`t create new user! Email already used.' });
         } else {
             await connection.execute(
-                `INSERT INTO users (username, email, password) VALUES (:1, :2, :3)`,
-                [username, email, password],
-                {autoCommit: true}
+                'INSERT INTO users (username, email, password) VALUES (?, ?, ?)',
+                [username, email, password]
             );
-            await connection.close();
-            res.status(200).json({message: 'Data received and stored successfully!'});
+            await connection.release();
+            res.status(200).json({ message: 'Data received and stored successfully!' });
         }
-
     } catch (error) {
         console.error('Error:', error.message);
-        res.status(500).json({error: 'Internal Server Error'});
+        res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
 app.post('/login', async (req, res) => {
-        const {email, password} = req.body;
+    const { email, password } = req.body;
 
-        try {
-            const connection = await oracledb.getConnection(dbConfig);
-            const result = await connection.execute(
-                `SELECT * FROM users WHERE email = :email AND password = :password`,
-                {email, password}
-            );
+    try {
+        const connection = await pool.getConnection();
+        const [rows] = await connection.execute(
+            'SELECT * FROM users WHERE email = ? AND password = ?',
+            [email, password]
+        );
 
-            if (result.rows.length > 0) {
-                const userData = {
-                    username: result.rows[0][0],
-                    email: result.rows[0][1],
-                    password: result.rows[0][2],
-                };
-                res.status(200).json({message: 'Success logging', user: userData});
-            } else {
-                res.status(400).json({error: 'Incorrect login or password'});
-            }
-
-            await connection.close();
-        } catch
-            (error) {
-            console.error('Error:', error.message);
-            res.status(500).send('Internal Server Error');
+        if (rows.length > 0) {
+            const userData = {
+                username: rows[0].username,
+                email: rows[0].email,
+                password: rows[0].password,
+            };
+            res.status(200).json({ message: 'Success logging', user: userData });
+        } else {
+            res.status(400).json({ error: 'Incorrect login or password' });
         }
+
+        await connection.release();
+    } catch (error) {
+        console.error('Error:', error.message);
+        res.status(500).send('Internal Server Error');
     }
-);
+});
 
 
 app.post('/change-username', async (req, res) => {
-    let {email, username} = req.body;
+    let { email, username } = req.body;
 
     try {
-        const connection = await oracledb.getConnection(dbConfig);
-        const updateResult = await connection.execute(
-            `UPDATE users SET username = :username WHERE email = :email`,
-            {username, email},
-            {autoCommit: true}
+        const connection = await pool.getConnection();
+        const [updateResult] = await connection.execute(
+            'UPDATE users SET username = ? WHERE email = ?',
+            [username, email]
         );
 
-        if (updateResult.rowsAffected > 0) {
-            res.status(200).json({message: 'Username successfully updated'});
+        if (updateResult.affectedRows > 0) {
+            res.status(200).json({ message: 'Username successfully updated' });
         } else {
-            res.status(404).json({error: 'User not found'});
+            res.status(404).json({ error: 'User not found' });
         }
 
-        await connection.close();
+        await connection.release();
     } catch (error) {
         console.error('Error:', error.message);
         res.status(500).send('Internal Server Error');
@@ -108,23 +106,22 @@ app.post('/change-username', async (req, res) => {
 });
 
 app.post('/change-password', async (req, res) => {
-    let {email, password} = req.body;
+    let { email, password } = req.body;
 
     try {
-        const connection = await oracledb.getConnection(dbConfig);
-        const updateResult = await connection.execute(
-            `UPDATE users SET password = :password WHERE email = :email`,
-            {password, email},
-            {autoCommit: true}
+        const connection = await pool.getConnection();
+        const [updateResult] = await connection.execute(
+            'UPDATE users SET password = ? WHERE email = ?',
+            [password, email]
         );
 
-        if (updateResult.rowsAffected > 0) {
-            res.status(200).json({message: 'Password successfully updated'});
+        if (updateResult.affectedRows > 0) {
+            res.status(200).json({ message: 'Password successfully updated' });
         } else {
-            res.status(404).json({error: 'User not found'});
+            res.status(404).json({ error: 'User not found' });
         }
 
-        await connection.close();
+        await connection.release();
     } catch (error) {
         console.error('Error:', error.message);
         res.status(500).send('Internal Server Error');
